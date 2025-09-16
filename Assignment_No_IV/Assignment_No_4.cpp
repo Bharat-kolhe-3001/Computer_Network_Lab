d5caefcd752e4391b490051bd4ac832f43eda555
Bharat kolhe <163564571+Bharat-kolhe-3001@users.noreply.github.com>
2025-09-16 22:01:15 +0530

    Update Assignment_No_4.cpp

--- Assignment_No_IV/Assignment_No_4.cpp
+++ Assignment_No_IV/Assignment_No_4.cpp
@@ -1,45 +1,107 @@
 #include <iostream>
+#include <string>
 #include <cmath>
 using namespace std;
 
+string decToBin(int n) {
+    string bin = "";
+    for (int i = 7; i >= 0; i--) {
+        bin += ((n >> i) & 1) ? '1' : '0';
+    }
+    return bin;
+}
+
+int binToDec(string bin) {
+    int val = 0;
+    for (int i = 0; i < 8; i++) {
+        val = val * 2 + (bin[i] - '0');
+    }
+    return val;
+}
+
+void splitIP(string ipStr, int arr[4]) {
+    string part = "";
+    int j = 0;
+    for (int i = 0; i <= ipStr.size(); i++) {
+        if (ipStr[i] == '.' || i == ipStr.size()) {
+            arr[j++] = stoi(part);
+            part = "";
+        } else {
+            part += ipStr[i];
+        }
+    }
+}
+
+void printIP(int arr[4]) {
+    cout << arr[0] << "." << arr[1] << "." << arr[2] << "." << arr[3];
+}
+
+int countBits(int mask[4]) {
+    int bits = 0;
+    bool stop = false;
+    for (int i = 0; i < 4 && !stop; i++) {
+        string bin = decToBin(mask[i]);
+        for (char c : bin) {
+            if (c == '1' && !stop) {
+                bits++;
+            } else {
+                stop = true;
+                break;
+            }
+        }
+    }
+    return bits;
+}
+
 int main() {
-    int ipClass, subnetBits;
+    string ipStr, maskStr;
+    int ip[4], mask[4], net[4], broad[4];
+    int first[4], last[4];
 
-    cout << "Subnetting Demonstration\n";
-    cout << "Enter IP Class (A=1, B=2, C=3): ";
-    cin >> ipClass;
+    cout << "Enter IP Address:- ";
+    cin >> ipStr;
+    cout << "Enter Subnet Mask:- ";
+    cin >> maskStr;
 
-    cout << "Enter number of bits borrowed for subnetting: ";
-    cin >> subnetBits;
+    splitIP(ipStr, ip);
+    splitIP(maskStr, mask);
 
-    int defaultMaskBits = 0;
-    if (ipClass == 1) defaultMaskBits = 8;  
-    else if (ipClass == 2) defaultMaskBits = 16; 
-    else if (ipClass == 3) defaultMaskBits = 24; 
-    else {
-        cout << "Invalid IP class!" << endl;
-        return 0;
+    for (int i = 0; i < 4; i++) {
+        net[i] = ip[i] & mask[i];
     }
 
-    int newMaskBits = defaultMaskBits + subnetBits;
-    int totalSubnets = pow(2, subnetBits);
-    int hostBits = 32 - newMaskBits;
+    for (int i = 0; i < 4; i++) {
+        broad[i] = net[i] | (~mask[i] & 255);
+    }
+
+    for (int i = 0; i < 4; i++) {
+        first[i] = net[i];
+        last[i] = broad[i];
+    }
+    first[3] += 1;
+    last[3] -= 1;
+
+    int subnetBits = countBits(mask);
+    int hostBits = 32 - subnetBits;
     int hostsPerSubnet = pow(2, hostBits) - 2;
+    int numberOfSubnets = pow(2, subnetBits);
 
-    cout << "\nResults:\n";
-    cout << "Default Mask Bits: " << defaultMaskBits << endl;
-    cout << "New Subnet Mask Bits: " << newMaskBits << endl;
-    cout << "Number of Subnets: " << totalSubnets << endl;
-    cout << "Hosts per Subnet: " << hostsPerSubnet << endl;
+    cout << "\nNetwork ID: ";
+    printIP(net);
 
-    // Display subnet mask in dotted decimal form
-    unsigned long mask = 0xFFFFFFFF << (32 - newMaskBits);
-    cout << "Subnet Mask: ";
-    for (int i = 3; i >= 0; i--) {
-        cout << ((mask >> (i * 8)) & 0xFF);
-        if (i != 0) cout << ".";
-    }
+    cout << "\nBroadcast Address: ";
+    printIP(broad);
+
+    cout << "\nHost Range: ";
+    printIP(first);
+    cout << " - ";
+    printIP(last);
+
+    cout << "\nNumber of Subnet Bits: " << subnetBits;
+    cout << "\nNumber of Host Bits: " << hostBits;
+    cout << "\nNumber of Hosts per Subnet: " << hostsPerSubnet;
+    cout << "\nNumber of Subnets possible: " << numberOfSubnets;
+
     cout << endl;
-
     return 0;
 }
