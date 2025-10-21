6da242c856de260e9a8b6efd0cf41b3967549a44
Bharat kolhe <163564571+Bharat-kolhe-3001@users.noreply.github.com>
2025-10-21 10:41:01 +0530

    Update SubnettingDemo.java

--- 04-Assignment/SubnettingDemo.java
+++ 04-Assignment/SubnettingDemo.java
@@ -6,7 +6,7 @@ public class SubnettingDemo {
         String bin = "";
         for (int i = 7; i >= 0; i--) {
             bin += ((n >> i) & 1) == 1 ? "1" : "0";
-        }
+        } 
         return bin;
     }
 
